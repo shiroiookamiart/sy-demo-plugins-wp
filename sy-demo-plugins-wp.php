846c1cd73664ced8b6d0cb3cd61ed75079de6fba
shiroiokamiart <dbeltran486@gmail.com>
2022-02-16 14:49:25 -0400

    modificaciones sy-demo-plugins-wp.php

---
+++ sy-demo-plugins-wp.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name: demo
+Plugin URI: https://system-adl.com
+Description: descricion
+Version: 1.0
+Author: System adl
+Author URI: https://system-adl.com
+License: GPL2
+*/
+
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
